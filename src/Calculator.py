a4e37e5e16566451f92d325843274875344c38bf
Jaime Rodriguez <jaime.rodriguez@sap.com>
2022-10-06 21:03:29 -0500

    Adiciono comentarios

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,8 +1,19 @@
+
+
 class Command:
-   def __init__(self,a,b):
-       self.a = a
-       self.b = b
- 
-   def execute(self):
-       raise Exception('Overwrite this one pls')
+	'''
+	Clase base para implemantar los commandos que ejecutan
+	las operaciones matematicas de la calculadora.
+	'''
+	def __init__(self,a,b):
+	   self.a = a
+	   self.b = b
+
+	def execute(self):
+	'''
+	Por favor sobreescribir este método en cada commando.
+	'''
+		raise Exception('Overwrite this one pls')
+
+# Implementar comandos
 
